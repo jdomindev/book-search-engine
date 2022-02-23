6bfe92c98e8d24bc8d753b0d8754a99e9f020a3a
Jose Dominguez <jdomindev@gmail.com>
2022-02-23 17:58:52 -0500

    worked on refactoring server side schemas, and added boiler plate server and App.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,74 @@
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+              return User.findOne({ _id: context.user._id });
+            }
+            throw new AuthenticationError('You need to be logged in!');
+          },
+    },
+
+    Mutation: {
+
+        login: async (parent, { email, username, password }) => {
+            const user = await User.findOne({ $or: [{ username }, { email }] });
+        
+            if (!user) {
+                throw new AuthenticationError('No user with this email found!');
+            }
+        
+            const correctPw = await user.isCorrectPassword(password);
+        
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password!');
+            }
+        
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { user, token };
+        },
+
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: body } },
+                    { new: true, runValidators: true }
+                );
+            }
+            
+            throw new AuthenticationError('You need to be logged in!');
+            
+        },
+
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: args.bookId } } },
+                    { new: true, runValidators: true }
+                );
+                if (!updatedUser) {
+                    return res.status(404).json({ message: "Couldn't find user with this id!" });
+                  }
+            }
+            
+            throw new AuthenticationError('You need to be logged in!');
+            
+        },  
+        
+    }
+}
+
+
+module.exports = resolvers;
